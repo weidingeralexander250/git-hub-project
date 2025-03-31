9322463f9fec63d7ccb0c89c36c58c9bf20dbf3e
weidingeralexander250 <weidingeralexander250@gmail.com>
2025-03-31 00:42:05 +0000

    Add 20.ts

---
+++ src/20.ts
@@ -0,0 +1,10 @@
+let i = 0;
+while (i < 10) {
+    console.log(`Line ${i + 1}:`);
+    if (i % 2 === 0) {
+        console.log("This line is even");
+    } else {
+        console.log("This line is odd");
+    }
+    i++;
+}
